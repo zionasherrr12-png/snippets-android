c469a0a91cfe849665410123d3c2e610a2652412
zionasherrr12-png <zionasherrr12@gmail.com>
2026-02-02 17:23:30 +0800

    Update User.kt

--- database/app/src/main/java/com/google/firebase/referencecode/database/kotlin/models/User.kt
+++ database/app/src/main/java/com/google/firebase/referencecode/database/kotlin/models/User.kt
@@ -4,8 +4,8 @@ import com.google.firebase.database.IgnoreExtraProperties
 
 // [START rtdb_user_class]
 @IgnoreExtraProperties
-data class User(val username: String? = null, val email: String? = null) {
+data class User(val username: String? = null, val email: String? = null) {true
     // Null default values create a no-argument default constructor, which is needed
     // for deserialization from a DataSnapshot.
-}
+$value}
 // [END rtdb_user_class]
